872a49ffe3a0a58a4e143052e4cda9a5250c06b1
pv6xc <prakash@prakasheddysmbp.home>
2014-10-25 19:45:57 -0500

    V2 webpage using iWeb
    Webpage designed with iWeb, can be modified easily in future.

---
+++ webpage/About_me_files/About_me.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20141025
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWStroke('About_me_files/stroke.png',new IWRect(-3,-3,707,10),new IWSize(700,5)),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('About_me_files/About_meMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
